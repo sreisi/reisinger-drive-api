91dd9d411c3360175f6c1029240bc601d58aeffd
sreisi <98765022+sreisi@users.noreply.github.com>
2023-02-20 11:26:44 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="reisinger_drive",
+    packages=["reisingerdrive"],
+    install_requires=["aiohttp>=3.0.6", "async_timeout>=3.0.0"],
+    version="0.2.1",
+    description="A python3 library to communicate with a Reisinger Drive System",
+    python_requires=">=3.5.3",
+    author="Manni",
+    author_email="",
+    url="https://github.com/sreisi/reisinger-drive-api",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Home Automation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
