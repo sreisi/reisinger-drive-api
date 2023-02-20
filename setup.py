823a03eccecdb9ed69210e6764006aee1f8f0443
sreisi <98765022+sreisi@users.noreply.github.com>
2023-02-20 12:16:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="reisinger_drive",
     packages=["reisingerdrive"],
     install_requires=["aiohttp>=3.0.6", "async_timeout>=3.0.0"],
     version="0.2.1",
     description="A python3 library to communicate with a Reisinger Drive System",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     python_requires=">=3.5.3",
     author="sreisi",
     author_email="",
